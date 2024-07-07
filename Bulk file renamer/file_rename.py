8d85aaf8d3905d485c2a65d9a5633d08c0945bc6
prathebaAT <pratheba63@gmail.com>
2024-07-07 14:24:55 +0530

    created bulk file rename project

---
+++ Bulk file renamer/file_rename.py
@@ -0,0 +1,28 @@
+import os
+
+def main():
+    i = 0
+    path = "C:/Users/atb/Documents/Testcheck/"
+    for filename in os.listdir(path):
+        my_file = "testname" + str(i) + ".txt"
+        my_src = path + filename
+        my_file = path + my_file
+        os.rename(my_src, my_file)
+        i+=1
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+# with open('data.txt' , 'w') as f:
+#     data = 'The web involves the internet'
+#     f.write(data)
+
+# try:
+#     with open("file.log") as file:
+#         read_data = file.read()
+# except:
+#     print("Couldn't open file.log")
